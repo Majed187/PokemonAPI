05e7ae4ffc25f2dfd96af9f0bb6f852600307fba
Majed Shrendan <majedshrndan45@gmail.com>
2019-02-15 18:12:40 +0100

    added new component

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class PokemonCard extends Component {
+  render() {
+    return (
+      <div
+        className="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div classNames="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalCenterTitle">
+                shsoui
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button type="button" className="" data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PokemonCard;
